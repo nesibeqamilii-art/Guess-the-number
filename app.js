6d02ea94e4c82b9726e9bcba27e244884189df2e
nesibeqamilii-art <nesibeqamilii@gmail.com>
2026-09-18 12:38:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Replace these two values with your project's values from Supabase > Project Settings > API. */
 const SUPABASE_URL = 'https://scilokziytpsbbecqofn.supabase.co';
-const SUPABASE_ANON_KEY = sb_publishable_FOGe2EZqOLg9NSgONVJ3gA_d6Gma1CA
+const const SUPABASE_ANON_KEY = 'sb_publishable_your-long-key-is-here';
 
 const { createClient } = window.supabase;
 const state = { client: null, room: null, playerSlot: null, channel: null };
